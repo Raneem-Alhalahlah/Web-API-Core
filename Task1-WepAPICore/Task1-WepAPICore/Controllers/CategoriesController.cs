02b6c42cc2996e3a57d9c6faa3a3a9c857e0514a
Raneem-Alhalahlah <raneemalhalahleh@gmail.com>
2024-08-19 17:56:47 +0300

    task1API

---
+++ Task1-WepAPICore/Task1-WepAPICore/Controllers/CategoriesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Task1_WepAPICore.Models;
+
+namespace Task1_WepAPICore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly MyDbContext _myDbContext;
+        public CategoriesController(MyDbContext myDbContext)
+        {
+            _myDbContext = myDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult getAllCategories() {
+
+            var categories = _myDbContext.Categories.ToList();  
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCategoryById(int id)
+        {
+
+            var CategoryById = _myDbContext.Categories.Where(c => c.CategoryId == id).FirstOrDefault();
+            return Ok(CategoryById);
+        }
+    }
+}
---
+++ Task1-WepAPICore/Task1-WepAPICore/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Task1_WepAPICore.Models;
+
+namespace Task1_WepAPICore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly MyDbContext _myDbContext;
+        public ProductsController(MyDbContext myDbContext)
+        {
+            _myDbContext = myDbContext;
+        }
+        [HttpGet]
+        public IActionResult getAllProducts()
+        {
+
+            var products = _myDbContext.Products.ToList();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+
+            var productById = _myDbContext.Products
+                                              .Include(p => p.Category)  
+                                              .FirstOrDefault(c => c.ProductId == id);
+            return Ok(productById);
+        }
+    }
+
+}
+
